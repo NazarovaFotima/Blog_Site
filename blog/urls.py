9e685eb42838127d4442a328c36410f485f30820
NazarovaFotima <akramovafotima1996@gmail.com>
2025-01-31 12:13:57 +0500

    add models, views, templates

--- blog/urls.py
+++ blog/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('', include('blogapp.urls')),
 ]
 
-urlpatterns+= static(settings.STATIC_URLS, document_root=settings.STATIC_ROOT)
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Post
+from django.views.generic import ListView
 
-def first_page(request):
-    html ="""
-    <h1>First Page</h1>
-    """
-    return HttpResponse(html)
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 5
+    template_name = "blog/post/list.html"
+
+
+
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(Post, slug=slug, status= 'published', publish__year=year, publish__month=month, publish__day=day,)
+    return render(request, 'blog/post/detail.html', {'post':post})
